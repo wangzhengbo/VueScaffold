f6b793150174be227b6cf576970c8a41c0d53e7d
wang zhengbo <wangzb4@lenovo.com>
2018-06-11 15:41:02 +0800

    注释修复

--- config/index.js
+++ config/index.js
@@ -69,7 +69,7 @@ module.exports = {
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
+    // `npm run build:report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.ANALYZER_REPORT === 'true'
   }
