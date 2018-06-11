a7dfbdfb1bf164e119fac7746402c1cf36e8e6d3
wang zhengbo <wangzb4@lenovo.com>
2018-06-11 13:37:37 +0800

    1. 增加Home页面，并将原有的Home页改成hello页；2. 路由使用history模式

---
+++ src/components/home/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <ul>
+      <li>
+        <router-link to="/hello">Hello</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+import Home from '@/components/home/index'
+import HelloWorld from '@/components/HelloWorld'
+
+export default [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/hello',
+    name: 'hello',
+    component: HelloWorld
+  }
+]
