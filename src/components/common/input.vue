e4cf07c6ca3783df1b534d47b5ee94f68dcc8e09
wang zhengbo <wangzb4@lenovo.com>
2018-06-13 21:51:02 +0800

    增加自定义组件v-model示例

---
+++ src/components/common/input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label v-if="label" :value="value" @input="onInput" @click="onLabelClick">{{ label }}</label>
+    <input ref="input">
+  </div>
+</template>
+<script>
+export default {
+  name: 'Input',
+  props: {
+    label: {
+      type: String,
+      require: false
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    onInput () {
+      this.$emit('input', this.value)
+    },
+    onLabelClick () {
+      this.$refs.input.focus()
+    }
+  }
+}
+</script>
--- src/components/component/index.vue
+++ src/components/component/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
+  <div class="ui-component">
     <ui-button size="big" @click="onClick('big')">size: big</ui-button>
     <ui-button size="medium" @click="onClick('medium')">size: medium</ui-button>
     <ui-button size="small" @click="onClick('small')">size: small</ui-button>
     <ui-button size="big" disabled @click="onClick('big')">size: big disabled</ui-button>
     <ui-button size="medium" disabled @click="onClick('medium')">size: medium disabled</ui-button>
     <ui-button size="small" disabled @click="onClick('small')">size: small disabled</ui-button>
+
+    <div class="container">
+      Input Without Label: {{ value1 }}
+      <ui-input v-model="value1" />
+    </div>
+
+    <div class="container">
+      Input Without Label: {{ value2 }}
+      <ui-input v-model="value2" label="名称" />
+    </div>
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      value1: '',
+      value2: ''
+    }
+  },
   methods: {
     onClick (size) {
       alert(size)
@@ -17,3 +33,10 @@ export default {
   }
 }
 </script>
+<style lang="less" scoped>
+  .ui-component {
+    > .container {
+      margin: 30px 0;
+    }
+  }
+</style>
